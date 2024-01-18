43a04885867545b5a8c96d7e5dbb95e09b01c3db
AksharaMukundSonale <91076766+AksharaMukundSonale@users.noreply.github.com>
2024-01-18 19:00:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+song1 = "";
+song2 = "";
 song = "";
-leftWristX = 0;
-leftWristY = 0;
-rightWristX = 0;
-rightWristY = 0;
 
 function preload()
 {
-	song = loadSound("music.mp3");
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("Something.mp3");
 }
 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
 
 function setup() {
 	canvas =  createCanvas(600, 500);
@@ -22,46 +29,60 @@ function setup() {
 }
 
 function modelLoaded() {
-	console.log('PoseNet Is Initialized');
-  }
+  console.log('PoseNet Is Initialized');
+}
 
-  function gotPoses(results)
+function gotPoses(results)
+{
+  if(results.length > 0)
   {
-	if(results.length > 0)
-	{
-	  console.log(results);
-	  scoreLeftWrist = results[0].pose.keypoints[9].score;
-	  console.log("Score of Left Wrist = " + scoreLeftWrist);
+	console.log(results);
+	scoreRightWrist = results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-	  leftWristX = results[0].pose.leftWrist.x;
-	  leftWristY = results[0].pose.leftWrist.y;
-	  console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
-
-	  rightWristX = results[0].pose.rightWrist.x;
-	  rightWristY = results[0].pose.rightWrist.y;
-	  console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
-
-	}
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
   }
-  
+}
 
 function draw() {
 	image(video, 0, 0, 600, 500);
 
-	fill("#7d294e");
-	stroke("#7d294e");
-
+	fill("#FF0000");
+	stroke("#FF0000");
+	song = song1.isPlaying()
 	if(scoreLeftWrist > 0.2)
+	{ 
+		circle(leftWristX,leftWristY,20);
+		song2.stop()
+		if(song == false)
+		{
+			song1.play()
+		}
+		else{
+			document.getElementById("song_id").innerHTML = "Song Name = Some"
+		}
+	}
+	if (scoreRightWrist > 0.2)
 	{
-		circle(leftWristX, leftWristY, 20);
-	 InNumberleftWristY = Number(leftWristY);
-	 remove_decimals = floor(InNumberleftWristY);
-	 volume = remove_decimals/500;
-	 document.getElementById("volume").innerHTML = "Volume = " + volume;
-	 song.setVolume(volume);
+		circle(rightWristX,rightWristY,20);
+		song1.stop()
+		if(song == false)
+		{
+			song2.play
+		}
+		else{
+			document.getElementById("song_id").innerHTML = "Song name = Alone"
+		}
 	}
 }
-
 
 function play()
 {
